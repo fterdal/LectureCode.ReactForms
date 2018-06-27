96edcd9f94519670b199f31a8050d08177e7586a
Finn Terdal <fterdal@gmail.com>
2018-06-27 16:36:39 -0500

    improve components by using arrow funcs and no constructor

--- react/index.js
+++ react/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-// This form is the default HTML form.
+// This form is the default HTML form. Try submitting it and looking at the address bar...
 class BasicForm extends React.Component {
   render() {
     return (
@@ -18,17 +18,15 @@ class BasicForm extends React.Component {
   }
 }
 
+// This is a little better! It prevents default HTML behavior, and keeps
+// track of form data in state. However, try changing state from React Dev tools
+// and you'll be sadly disappointed :-(
 class StatefulForm extends React.Component {
-  constructor() {
-    super()
-    this.state = { email: '' }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-  handleChange(event) {
+  state = { email: '' }
+  handleChange = (event) => {
     this.setState({ email: event.target.value })
   }
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault()
     console.log('form submitted with data:', this.state)
   }
@@ -46,17 +44,21 @@ class StatefulForm extends React.Component {
   }
 }
 
+// The user's input is sent directly to state. Only when the state changes does
+// the input element change. This means when we change the state on the component
+// the form reacts!
 class ControlledForm extends React.Component {
-  constructor() {
-    super()
-    this.state = { email: '' }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-  handleChange(event) {
+  // constructor() {
+  //   super()
+  //   this.state = { email: '' }
+  //   this.handleChange = this.handleChange.bind(this)
+  //   this.handleSubmit = this.handleSubmit.bind(this)
+  // }
+  state = { email: '' }
+  handleChange = (event) => {
     this.setState({ email: event.target.value })
   }
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault()
     console.log('form submitted with data:', this.state)
   }
@@ -77,8 +79,8 @@ class ControlledForm extends React.Component {
 const App = () => (
   <div className="container">
     {/* <BasicForm /> */}
-    <StatefulForm />
-    {/* <ControlledForm /> */}
+    {/* <StatefulForm /> */}
+    <ControlledForm />
   </div>
 )
 
