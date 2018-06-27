0e80a0a29913f53897b6ed4c7fe626c7f12240dd
Finn Terdal <fterdal@gmail.com>
2018-06-27 16:02:41 -0500

    fixes handleSubmit on stateful form, and completes controlled form

--- react/index.js
+++ react/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+// This form is the default HTML form.
 class BasicForm extends React.Component {
   render() {
     return (
@@ -11,8 +12,7 @@ class BasicForm extends React.Component {
           <input type="text" name="email" />
           <button type="submit">Submit</button>
         </form>
-        <iframe src="https://giphy.com/embed/4hdWKFCCKcsbC" />
-        {/* <iframe src="https://giphy.com/embed/3oEduY2IBr0s8AdrIk" frameBorder="0" /> */}
+        <iframe src="https://giphy.com/embed/4hdWKFCCKcsbC" frameBorder="0" />
       </div>
     )
   }
@@ -23,16 +23,49 @@ class StatefulForm extends React.Component {
     super()
     this.state = { email: '' }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleChange(event) {
     this.setState({ email: event.target.value })
   }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log('form submitted with data:', this.state)
+  }
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email:</label>
           <input type="text" name="email" onChange={this.handleChange} />
+          <button type="submit">Submit</button>
+        </form>
+        <iframe src="https://giphy.com/embed/3oEduY2IBr0s8AdrIk" frameBorder="0" />
+      </div>
+    )
+  }
+}
+
+class ControlledForm extends React.Component {
+  constructor() {
+    super()
+    this.state = { email: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({ email: event.target.value })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log('form submitted with data:', this.state)
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input type="text" name="email" onChange={this.handleChange} value={this.state.email} />
           <button type="submit">Submit</button>
         </form>
         <img src="/drake-does-react.png" />
@@ -41,12 +74,11 @@ class StatefulForm extends React.Component {
   }
 }
 
-
-
 const App = () => (
   <div className="container">
-    <BasicForm />
-    {/* <StatefulForm /> */}
+    {/* <BasicForm /> */}
+    <StatefulForm />
+    {/* <ControlledForm /> */}
   </div>
 )
 
