fa464876760e291fd2875cfaa931c8e70e156906
Finn Terdal <fterdal@gmail.com>
2018-06-27 14:24:16 -0500

    got a basic form

--- react/index.js
+++ react/index.js
@@ -1,22 +1,24 @@
+/* eslint react/prefer-stateless-function:0 */
 import React from 'react'
 import { render } from 'react-dom'
 
+class BasicForm extends React.Component {
+  render() {
+    return (
+    <form>
+        <input type="text" name="email" />
+        <button type="submit" />
+    </form>)
+  }
+}
+
 const App = () => (
   <div>
-    <h1>Hello from React!</h1>
-    <p>Riddle: We come at night without being fetched; we disappear by day without being stolen. What are we?</p>
-    <p style={{fontStyle: 'italic'}}>Pop open the developer tools to see the answer.</p>
+    <BasicForm />
   </div>
 )
-
-console.log(`Answer:
-✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
-✨ ✨ ✨ Stars ✨ ✨ ✨
-✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
-`)
 
 render(
   <App />,
   document.getElementById('app')
 )
-
